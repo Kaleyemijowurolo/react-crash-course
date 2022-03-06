d335d440f86f23d8511e367f02dcf3e85d630c59
Kayode Kaleyemi <caresskakka@yahoo.com>
2022-03-06 12:20:03 +0100

    updated button, footer and input component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import Footer from "./components/Footer";
+
+const AppWrapper = styled("div")`
+  width: 100vw;
+  display: flex;
+  border: solid;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
 
 const App = () => {
-  return <div>Welcome</div>;
+  return (
+    <AppWrapper>
+      <Footer />
+    </AppWrapper>
+  );
 };
 
 export default App;
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,8 +1,15 @@
 import React from "react";
-import {} from "./input.styles";
+import { StyledInput, InputWrapper } from "./input.styles";
 
-const Input = () => {
-  return <div>index</div>;
-};
-
+const Input = ({ text, password, label, width, height, placeholder }) => (
+  <InputWrapper>
+    <label>{label}</label>
+    <StyledInput
+      type={text ? "text" : password ? "password" : ""}
+      placeholder={placeholder}
+      width={width}
+      height={height}
+    />
+  </InputWrapper>
+);
 export default Input;
