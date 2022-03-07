025a14d57a2f10cac49077233eb221cdb2c1dbdd
mayowa86 <mayowasamuel86@gmail.com>
2022-03-07 02:37:52 +0100

    updates

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { FormStyle, Card } from "./form.styles";
+import { Link } from "react-router-dom";
 
-const Form = ({ signIn, signUp }) => {
+const Form = ({ signIn, signUp, onClick }) => {
 	return (
 		<FormStyle signIn={signIn}>
 			<Card signIn={signIn}>
-				<header>{signIn ? <>{'Welcome'} &#129309;&#127995;</> : "Create Account with Us"} </header>
+				<header>
+					{signIn ? (
+						<>{"Welcome"} &#129309;&#127995;</>
+					) : (
+						"Create Account with Us"
+					)}{" "}
+				</header>
 				<main>
 					<div className="username_pass">
 						<div className="input_wrapper">
@@ -20,42 +27,70 @@ const Form = ({ signIn, signUp }) => {
 					{/* ----------------------------------------------------- */}
 					{signIn ? null : (
 						<>
-						<div className="mini_input">
-							<div className="input_wrapper">
-								<Input text label={"first name"} placeholder={"John"} />{" "}
+							<div className="mini_input">
+								<div className="input_wrapper">
+									<Input text label={"first name"} placeholder={"John"} />{" "}
+								</div>
+								<div className="input_wrapper">
+									<Input text label={"last name"} placeholder={"Doe"} />{" "}
+								</div>
 							</div>
-							<div className="input_wrapper">
-								<Input text label={"last name"} placeholder={"Doe"} />{" "}
+
+							{/* ----------------------------------------------------- */}
+
+							<div className="radio">
+								<div className="input_wrapper">
+									<input type="radio" name="choice" id="male" value={"male"} />
+									<label htmlFor="male">{"Male"}</label>
+								</div>
+
+								<div className="input_wrapper">
+									<input
+										type="radio"
+										name="choice"
+										id="female"
+										value={"female"}
+									/>
+									<label htmlFor="female">{"Female"}</label>
+								</div>
 							</div>
-						</div>
-					
-					{/* ----------------------------------------------------- */}
+							{/* ----------------------------------------------------- */}
 
-					<div className="radio">
-						<div className="input_wrapper">
-							<input type="radio" name="choice" id="male" value={"male"} />
-							<label htmlFor="male">{"Male"}</label>
-						</div>
-
-						<div className="input_wrapper">
-							<input type="radio" name="choice" id="female" value={"female"} />
-							<label htmlFor="female">{"Female"}</label>
-						</div>
-					</div>
-					{/* ----------------------------------------------------- */}
-
-					<div className="news_letter">
-						<div className="input_wrapper">
-							<input type="checkbox" name="newletter" id="" />
-							<label htmlFor="female">{"I want to receive newsletter"}</label>
-						</div>
-					</div>
-					</>)}
+							<div className="news_letter">
+								<div className="input_wrapper">
+									<input type="checkbox" name="newletter" id="" />
+									<label htmlFor="female">
+										{"I want to receive newsletter"}
+									</label>
+								</div>
+							</div>
+						</>
+					)}
 				</main>
 
 				<footer>
-					<Button style={{ width: "100%" }} primary text={signIn? "Sign In" : "Create Account"} />
+					<Link to={"/sign-in"}>
+						<Button
+							onClick={onClick}
+							style={{ width: "100%" }}
+							primary
+							text={signIn ? "Sign In" : "Create Account"}
+						/>
+					</Link>
 				</footer>
+				<div style={{ float: "right" }}>
+					{signIn ? (
+						<>
+							{"I Dont have an account?"}
+							<Link to={"/sign-up"}> {"Create Account"}</Link>
+						</>
+					) : (
+						<>
+							{"Already have an account?"}
+							<Link to={"/sign-in"}> {"Sign In"}</Link>
+						</>
+					)}
+				</div>
 			</Card>
 		</FormStyle>
 	);
--- src/screens/sign-up/index.jsx
+++ src/screens/sign-up/index.jsx
@@ -3,11 +3,14 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { SignUpStyle, Card } from "./signUp.styles";
 import Form from "../../components/Form";
+import { Router, useRoutes } from "react-router-dom";
+
 
 const SignUp = () => {
+  Router= new useRoutes()
 	return (
 		<SignUpStyle>
-		<Form />  
+		<Form onClick={() => Router.push("dashboard")} />  
 		</SignUpStyle>
 	);
 };
